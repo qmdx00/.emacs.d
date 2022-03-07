2e40c2959d2f598a866cb5aa0b001108219d72c1
wimi <ywm00@qq.com>
2022-03-07 16:18:22 +0800

    feat: add custom files

--- init.el
+++ init.el
@@ -1,11 +1,17 @@
-;; packagerepository
+;; package repository
 (require 'package)
 (setq package-archives '(
   ("gnu"   . "http://elpa.emacs-china.org/gnu/")
   ("melpa" . "http://elpa.emacs-china.org/melpa/")))
 (package-initialize)
 
-;; bootstrapuse-package
+;; custom config
+(setq custom-file "~/.emacs.d/custom.el")
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
+(load custom-file)
+
+;; bootstrap use-package
 (unless 
     (package-installed-p 'use-package)
     (package-refresh-contents)
@@ -16,17 +22,5 @@
 ;; load evil
 (use-package evil
   :demand t
-  :ensure t
   :config (evil-mode 1))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
