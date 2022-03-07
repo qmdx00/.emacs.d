3aa234de15935bbd5323b3d5e653e9a40be9f78d
wimi <ywm00@qq.com>
2022-03-07 17:54:47 +0800

    feat: use packages

--- init.el
+++ init.el
@@ -13,9 +13,9 @@
 
 ;; bootstrap use-package
 (unless 
-    (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
+  (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 (require 'use-package)
 (setq use-package-always-ensure t)
 
@@ -23,4 +23,30 @@
 (use-package evil
   :demand t
   :config (evil-mode 1))
+
+;; load ivy
+(use-package ivy
+  :config (ivy-mode 1))
+
+;; load company
+(use-package company
+  :hook (after-init . global-company-mode)
+  :config
+  (setq company-tooltip-align-annotations t 
+        company-tooltip-limit 20           
+        company-show-numbers t            
+        company-idle-delay .2            
+        company-minimum-prefix-length 1 
+        ))
+
+;; load projectile
+(use-package projectile
+  :config (projectile-mode 1)
+  :bind (:map project-command-map
+	      ("C-c p" . projectile-command-map)))
+
+;; load magit
+(use-package magit
+	    :bind (("C-x g" . magit-status)
+		   ("C-x C-g" . magit-status)))
 
