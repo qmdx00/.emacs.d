797cc0bb41ac6f4cc22805d6f47e85209924beaa
wimi <ywm00@qq.com>
2023-10-03 15:40:27 +0800

    code optimize
    Signed-off-by: wimi <ywm00@qq.com>

--- lisp/init-kbd.el
+++ lisp/init-kbd.el
@@ -7,11 +7,7 @@
 ;; hydra
 (use-package hydra)
 
-;; hydra kbd
-(global-set-key (kbd "C-c f") 'hydra-frame/body)
-(global-set-key (kbd "C-c s") 'hydra-search/body)
-
-;; keybindings
+;; frame
 (defhydra hydra-frame (:exit t)
   "Emacs Frame Management."
   ("f" other-frame "other frame")
@@ -19,25 +15,36 @@
   ("n" make-frame-command "new frame")
   ("t" tear-off-window "tearoff window"))
 
+;; search
 (defhydra hydra-search (:exit t)
   "Emacs Search Keys"
   ("s" swiper "search buffer")
   ("g" counsel-git-grep "search git project")
   ("d" counsel-rg "serach directory"))
 
-;; graphic kbd
-(when (display-graphic-p)
-  (global-set-key (kbd "s-b") 'switch-to-buffer)
-  (global-set-key (kbd "s-<return>") 'toggle-frame-fullscreen)
-  (global-set-key (kbd "s-<mouse-1>") 'xref-find-definitions-at-mouse)
-  (global-set-key (kbd "s-<mouse-3>") 'xref-find-references-at-mouse))
+;; global kbd
+(let ((keybindings-alist
+       '(("C-c f" . hydra-frame/body)
+	 ("C-c s" . hydra-search/body)
+	 ("M-x" . counsel-M-x)
+	 ("M-n" . switch-to-next-buffer)
+	 ("M-p" . switch-to-prev-buffer)
+	 ("C->" . mc/mark-next-like-this)
+	 ("C-<" . mc/mark-previous-like-this)
+	 ("C-c C-SPC" . mc/mark-all-like-this))))
+  (dolist (item keybindings-alist)
+    (global-set-key (kbd (car item)) (cdr item))))
 
-;; global kdb
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "M-n") 'switch-to-next-buffer)
-(global-set-key (kbd "M-p") 'switch-to-prev-buffer)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-SPC") 'mc/mark-all-like-this)
+;; graphic global kbd
+(let ((graphic-keybindings-alist
+       '(("s-b" . switch-to-buffer)
+	 ("s-<return>" . toggle-frame-fullscreen)
+	 ("s-r" . restart-emacs)
+	 ("s-<mouse-1>" . xref-find-definitions-at-mouse)
+	 ("s-<mouse-3>" . xref-find-references-at-mouse))))
+  (dolist (item graphic-keybindings-alist)
+    (when (display-graphic-p)
+      (global-set-key (kbd (car item)) (cdr item)))))
+
 
 (provide 'init-kbd)
